eb7f2764a47355ef9cbc6d66ae6ca2c124000b1d
Brett Sullivan <opticalfalicy@gmail.com>
2018-05-02 16:20:38 -0500

    Added axios

--- src/App.js
+++ src/App.js
@@ -1,21 +1,135 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios';
+
+import Sidebar from './Sidebar';
+import NoteList from './NoteList';
+import CreateNote from './CreateNote';
+import NoteView from './NoteView';
+import EditNote from './EditNote';
+
 import './App.css';
 
-class App extends Component {
+
+export default class App extends React.Component {
+  nextId = 0;
+  noteIndex = 0;
+
+  state = {
+    notes: [],
+  };
+
+  handleNoteViewIndex = inputId => {
+    for (let i = 0; i < this.state.notes.length; i++) {
+      if (this.state.notes[i].id === inputId) this.noteIndex = i;
+    };
+  };
+
+  handleCreateNote = inputNote => {
+    const newNote = {
+      id: this.nextId++,
+      title: inputNote.title,
+      body: inputNote.body,
+    };
+    const newNotes = [...this.state.notes, newNote];
+
+    axios
+    .post('https://swedishgood-note.herokuapp.com/notes/new', newNote)
+    .then(response => {
+      console.log(response);
+      this.setState({
+        notes: newNotes
+      });
+    })
+
+    axios.get('https://swedishgood-note.herokuapp.com/notes')
+    .then(response => {
+      this.setState({ notes: response.data })
+    })
+
+
+    
+    // const newNotes = [...this.state.notes, newNote];
+    // this.setState({
+    //   notes: newNotes,
+    // });
+  };
+
+  handleEditNote = inputNote => {
+    const editedNote = {
+      id: inputNote.id,
+      title: inputNote.title,
+      body: inputNote.body,
+    };
+    const editedNotes = [...this.state.notes];
+
+    axios
+    .put('https://swedishgood-note.herokuapp.com/notes/edit/:id', editedNote)
+    .then(res => {
+      this.setState({
+        notes: editedNotes
+      })
+    })
+
+    axios.get('https://swedishgood-note.herokuapp.com/notes')
+    .then(response => {
+      this.setState({ notes: response.data })
+    })
+    // editedNotes.splice(this.noteIndex, 1, editedNote);
+    // this.setState({
+    //   notes: editedNotes,
+    // });
+  };
+
+  handleDeleteNote = inputId => {
+    const id = inputId;
+
+    axios.delete('https://swedishgood-note.herokuapp.com/notes/:id')
+    .then(
+      console.log(`${id} deleted`)
+    )
+    axios.get('https://swedishgood-note.herokuapp.com/notes')
+    .then(response => {
+      this.setState({ notes: response.data })
+    })
+  };
+
+
+    // const lessNotes = this.state.notes.filter(note => note.id !== inputId);
+
+
+    // this.setState({
+    //   notes: lessNotes,
+    // });
+  
+
+  updateSortedNotes = sortedNotes => {
+    this.setState({
+      notes: sortedNotes,
+    })
+  }
+
+  componentDidMount(){
+    axios
+    .get('https://swedishgood-note.herokuapp.com/notes')
+    .then(response =>{
+      this.setState(() => {notes: response.data})
+    })
+
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <Sidebar />
+          <Route exact path={"/"} render={() => <NoteList notes={this.state.notes} handleNoteViewIndex={this.handleNoteViewIndex} updateSortedNotes={this.updateSortedNotes}/>} />
+          <Route exact path={"/create"} render={() => <CreateNote createNote={this.handleCreateNote} />} />
+          <Route exact path={"/view"} render={() => <NoteView note={this.state.notes[this.noteIndex]} toggleModal={this.toggleModal} handleDeleteNote={this.handleDeleteNote} />} />
+          <Route exact path={"/edit"} render={() => <EditNote note={this.state.notes[this.noteIndex]} handleEditNote={this.handleEditNote} />} />
+        </div>
+      </Router>
     );
-  }
+  };
 }
-
-export default App;
