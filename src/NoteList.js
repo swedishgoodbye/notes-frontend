8ab923efd60230ec15385f2d8b5ffadf03f76cef
Brett Sullivan <opticalfalicy@gmail.com>
2018-05-03 14:40:41 -0500

    error searching

--- src/NoteList.js
+++ src/NoteList.js
@@ -25,7 +25,6 @@ export default class NoteList extends React.Component {
     return (
       <div className="YourNotes">
         <h2 className="SectionTitle">Your Notes:</h2>
-          <CSVLink className="YourNotes-CSV" data={this.state.notes} filename={"lambda-notes.csv"}>Download CSV</CSVLink>
         {this.props.notes.map(note => {
           <div>
           <div>{note.title}</div>
