04091efd1e4682c9f638f470d57df4edfd26a57b
Brett Sullivan <opticalfalicy@gmail.com>
2018-05-03 14:32:26 -0500

    error searching

--- src/NoteList.js
+++ src/NoteList.js
@@ -9,9 +9,7 @@ import './NoteList.css';
 export default class NoteList extends React.Component {
 
 
-  state = {
-    notes: this.state.notes,
-  };
+
 
 
 
@@ -26,8 +24,11 @@ export default class NoteList extends React.Component {
       <div className="YourNotes">
         <h2 className="SectionTitle">Your Notes:</h2>
           <CSVLink className="YourNotes-CSV" data={this.state.notes} filename={"lambda-notes.csv"}>Download CSV</CSVLink>
-        {this.props.notes.map(note =>{
-          <div>{...note}</div>
+        {this.props.notes.map(note => {
+          <div>
+          <div>{note.title}</div>
+          <div>{note.content}</div>
+          </div>
         })}
       </div>
     );
