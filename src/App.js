27a8e6715e33eb513f36a5249bcbafebbeb111fe
Brett Sullivan <opticalfalicy@gmail.com>
2018-05-03 21:45:27 -0500

    Log Test

--- src/App.js
+++ src/App.js
@@ -185,6 +185,7 @@ export default class App extends React.Component {
     .then(response =>{
       this.setState({notes: response.data})
       console.log(this.state.notes);
+      console.log(this.state.notes._id);
     })
 
   }
